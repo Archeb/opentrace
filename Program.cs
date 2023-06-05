47f9fb611e5110b14c0d5c11435256d18468d1eb
Archeb <archebasic@hotmail.com>
2023-06-05 01:28:35 +0100

    localized map provider setting

--- Program.cs
+++ Program.cs
@@ -15,9 +15,21 @@ namespace OpenTrace
         {
             UserSettings.LoadSettings();
             
-            if (UserSettings.language != null)
+            if (UserSettings.language != "")
             {
                 System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo(UserSettings.language);
+            }
+            if(UserSettings.mapProvider == "")
+            {
+                // 本地化地图供应商设置
+                if (System.Threading.Thread.CurrentThread.CurrentUICulture.Name == "zh-CN")
+                {
+                    UserSettings.mapProvider = "baidu"; 
+                }
+                else
+                {
+                    UserSettings.mapProvider = "google";
+                }
             }
             
             new Application(Eto.Platform.Detect).Run(new MainForm());
