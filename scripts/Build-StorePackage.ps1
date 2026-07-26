b3bdc50cefe76c64434d3be06d53b9c4c632612f
Archeb <archebasic@hotmail.com>
2026-07-26 17:45:29 +0800

    chore: bump version to 1.5.2

--- scripts/Build-StorePackage.ps1
+++ scripts/Build-StorePackage.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^\d+\.\d+\.\d+\.\d+$')]
-    [string]$Version = '1.5.1.0',
+    [string]$Version = '1.5.2.0',
 
     [string]$OutputDirectory = 'artifacts\store',
 
